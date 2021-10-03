75b39a6cb1b3ca8d5296c3f9f10974d693a01951
Mario Nakazato <marionakazat@gmail.com>
2021-10-03 18:59:59 -0300

    Build para criar executavel

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 
 set path="c:\Arquivos de Programas\winrar\";%path%
 
-winrar.exe a cf.zip conf.lua main.lua
+winrar.exe a cf.zip conf.lua main.lua palavra.lua cobra.lua comida.lua
 
 rename cf.zip game.love
 
